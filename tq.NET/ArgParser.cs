a044d52ca1da05e1258c364ebaa4c01aff33ee77
Dennis Greiner <d.greiner@highvillage.de>
2015-08-19 14:48:32 +0200

    implemented mono.options as argument parser

--- tq.NET/ArgParser.cs
+++ tq.NET/ArgParser.cs
@@ -3,37 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Mono.Options;
 
 namespace tq.NET {
     class ArgParser {
         public static List<Query> parse(string[] args) {
-            string arg;
+            var option_set = new OptionSet();
+            var help = false;
             var querylist = new List<Query>();
 
-            for (int i = 0; i < args.Length; i++) {
-                if (args[i] == "/s") {
-                    arg = args[i] + " " + args[i + 1];
-                    i++;
-                }
-                else {
-                    arg = args[i];
-                }
-                if (arg == "/f") {
-                    querylist.Add(new FeaturedStream());
-                }
-                else if (arg == "/t") {
-                    querylist.Add(new TopGame());
-                }
-                else if (arg == "/D") {
-                    foreach (var flag in args) {
-                        Console.WriteLine(flag);
-                    }
-                }
-                else if (arg.StartsWith("/s")) {
-                    var flags = arg.Split(' ');
-                    querylist.Add(new Search(flags[1]));
-                }
-            }
+            option_set.Add("?|help|h", "Prints this help message", option => help = option != null);
+            option_set.Add("f|F|featured", "Shows the featured Streams", option => querylist.Add(new FeaturedStream()));
+            option_set.Add("t|T|top|top-games", "Shows the Top Games sorted by viewers", option => querylist.Add(new TopGame()));
+            option_set.Add("s=|search=", "Searches for a stream", option => querylist.Add(new Search(option)));
+
+            option_set.Parse(args);
             return querylist;
         }
     }
