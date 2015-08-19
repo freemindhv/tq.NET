37ade0a23a8b1c9fa951b62d1a147cfe0a762aa6
Dennis Greiner <d.greiner@highvillage.de>
2015-08-19 15:37:02 +0200

    formatting

--- tq.NET/ArgParser.cs
+++ tq.NET/ArgParser.cs
@@ -12,13 +12,32 @@ namespace tq.NET {
             var help = false;
             var querylist = new List<Query>();
 
-            option_set.Add("?|help|h", "Prints this help message", option => help = option != null);
-            option_set.Add("f|F|featured", "Shows the featured Streams", option => querylist.Add(new FeaturedStream()));
-            option_set.Add("t|T|top|top-games", "Shows the Top Games sorted by viewers", option => querylist.Add(new TopGame()));
-            option_set.Add("s=|search=", "Searches for a stream", option => querylist.Add(new Search(option)));
+            option_set.Add("?|help|h", "Prints this help message",
+                option => help = option != null);
 
-            option_set.Parse(args);
+            option_set.Add("f|F|featured", "Shows the featured Streams",
+                option => querylist.Add(new FeaturedStream()));
+
+            option_set.Add("t|T|top|top-games",
+                "Shows the Top Games sorted by viewers",
+                option => querylist.Add(new TopGame()));
+
+            option_set.Add("s=|search=", "Searches for a stream",
+                option => querylist.Add(new Search(option)));
+
+            try {
+                option_set.Parse(args);
+            }
+            catch (OptionException) {
+                show_help("Error - usage is:", option_set);
+            }
             return querylist;
+        }
+
+        public static void show_help(string message, OptionSet option_set) {
+            Console.Error.WriteLine(message);
+            option_set.WriteOptionDescriptions(Console.Error);
+            Environment.Exit(-1);
         }
     }
 }
